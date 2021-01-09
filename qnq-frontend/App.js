508d5331d25b68ba1bb50d740f82924dbcb64a63
indra <indranilbit@outlook.com>
2021-01-09 23:29:20 +0530

    Onboard

--- qnq-frontend/App.js
+++ qnq-frontend/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import QnQDrawer from "./app/config/navigation";
+import Onboarding from "./app/screens/OnboardingScreen"
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <QnQDrawer />
+        <Stack.Navigator
+            headerMode= "none"
+            // initialRouteName={"Home"}   // Home Screen to be added later
+            >
+            <Stack.Screen name="Onboarding" component={Onboarding}/>
+            {/*<QnQDrawer />*/}
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
